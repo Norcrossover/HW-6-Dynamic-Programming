b92463d7bc6cd2103aa0f4d30489e601d134511f
Kobe Norcross <59990490+Norcrossover@users.noreply.github.com>
2023-03-14 23:12:35 +0000

    Succesfully completed the memoization and tabulation of the minimum sum descent.

--- .ccls-cache/@home@runner@HW-6-Dynamic-Programming/main.c
+++ .ccls-cache/@home@runner@HW-6-Dynamic-Programming/main.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,27 +6,31 @@
 // helpers
 int create_random_number(int max_value);
 int get_array_size();
-int create_matrix(int n, int matrix[n][n]);
+void create_matrix(int n, int matrix[n][n]);
 void set_matrix(int n, int matrix[n][n]);
 void print_matrix(int n, int matrix[n][n]);
 void print_array(int n, int array[]);
+int minimum(int a, int b);
 
 // minimum sum descent functions
 int minimum_sum_descent(int n, int matrix[n][n], int row, int column);
 int memoized_minimum_sum_descent(int n, int table[n][n], int matrix[n][n],
                                  int row, int column);
+int tabulated_minimum_sum_descent(int n, int matrix[n][n], int row, int column);
 void minimum_sum_descent_driver();
 
 // N choose K functions
-void nck_driver();
-int nck(int n, int k);
-int memoized_nck(int n, int k, int table[n][k]);
+void choose_driver();
+int choose(int n, int k);
+int memoized_choose(int n, int k, int table[n][k]);
+int tabulated_choose(int n, int k);
 
 // Rod Cutting Problem
 void create_rod(int n, int rod[]);
 void rod_cutting_driver();
 int rod_cutting(int n, int i, int list[]);
-int memoized_rod_cutting(int n, int lsit[]);
+int memoized_rod_cutting(int n, int list[]);
+int tabulated_rod_cutting(int n, int list[]);
 
 //===================================================================
 int main(void) {
@@ -34,7 +39,7 @@ int main(void) {
   minimum_sum_descent_driver();
   printf("\n-----------------------------------------\n\n");
   printf("N CHOOSE K \n\n");
-  nck_driver();
+  choose_driver();
   printf("\n-----------------------------------------\n\n");
   printf("ROD CUTTING PROBLEM \n\n");
   rod_cutting_driver();
@@ -43,35 +48,6 @@ int main(void) {
   return 0;
 }
 //===================================================================
-
-//-------------------------------------------------------------------------
-//                          Minimum Sum Descent
-//-------------------------------------------------------------------------
-
-void minimum_sum_descent_driver() {
-  int n = 10;
-  int matrix[n][n];
-  int table[n][n];
-  int min_sum = 0;
-  int memo_min_sum = 0;
-
-  // set matrices
-  set_matrix(n, matrix);
-  set_matrix(n, table);
-  create_matrix(n, matrix);
-
-  // set table
-  table[0][0] = matrix[0][0];
-  table[1][0] = matrix[1][0];
-  table[1][1] = matrix[1][1];
-
-  // print solutions
-  print_matrix(n, matrix);
-  min_sum = minimum_sum_descent(n, matrix, 0, 0);
-  memo_min_sum = memoized_minimum_sum_descent(n, table, matrix, 0, 0);
-  printf("\nThe minimum sum descent: %d\n\n", min_sum);
-  printf("The memoized minimum sum descent: %d\n\n", memo_min_sum);
-}
 
 int create_random_number(int max) { return floor(max * drand48()); }
 
@@ -84,7 +60,7 @@ int get_array_size() {
   return size;
 }
 
-int create_matrix(int n, int matrix[n][n]) {
+void create_matrix(int n, int matrix[n][n]) {
   int max_size_value = 10;
   for (int i = 0; i < n; i++) {
     for (int j = 0; j <= i; j++) {
@@ -117,53 +93,113 @@ void print_array(int n, int array[]) {
   printf("\n");
 }
 
+int minimum(int a, int b) { return ((a < b) ? a : b); }
+
+//-------------------------------------------------------------------------
+//                          Minimum Sum Descent
+//-------------------------------------------------------------------------
+
+void minimum_sum_descent_driver() {
+  int n = 10;
+  int matrix[n][n];
+  int table[n][n];
+  int min_sum = 0;
+  int memo_min_sum = 0;
+  int tab_min_sum = 0;
+
+  // naiive
+  set_matrix(n, matrix);
+  set_matrix(n, table);
+  create_matrix(n, matrix);
+  print_matrix(n, matrix);
+  min_sum = minimum_sum_descent(n, matrix, 0, 0);
+
+  // memoize
+  set_matrix(n, table);
+  memo_min_sum = memoized_minimum_sum_descent(n, table, matrix, 0, 0);
+
+  // tabular
+  tab_min_sum = tabulated_minimum_sum_descent(n, matrix, 0, 0);
+
+  printf("\nThe minimum sum descent: %d\n\n", min_sum);
+  printf("The memoized minimum sum descent: %d\n\n", memo_min_sum);
+  printf("The tabulated minimum sum descent: %d\n\n", tab_min_sum);
+}
+
 int minimum_sum_descent(int n, int matrix[n][n], int row, int col) {
-  int minimum = 0;
-  int left = col;
-  int right = col + 1;
-  int one = 0;
-  int two = 0;
+  int a = 0;
+  int b = 0;
 
   if (row == n - 1)
     return matrix[row][col];
 
-  one = minimum_sum_descent(n, matrix, row + 1, left);
-  two = minimum_sum_descent(n, matrix, row + 1, right);
+  a = minimum_sum_descent(n, matrix, row + 1, col);
+  b = minimum_sum_descent(n, matrix, row + 1, col + 1);
 
-  if (one < two) {
-    minimum = one;
-  } else {
-    minimum = two;
-  }
-
-  return matrix[row][col] + minimum;
+  return matrix[row][col] + ((a < b) ? a : b);
 }
 
 int memoized_minimum_sum_descent(int n, int table[n][n], int matrix[n][n],
                                  int row, int col) {
-  int minimum = 0;
-  int left = col;
-  int right = col + 1;
-  int one = 0;
-  int two = 0;
+  int a = 0;
+  int b = 0;
 
-  if (table[row][col] != -1)
+  if (row == n - 1)
     return matrix[row][col];
 
-  one = memoized_minimum_sum_descent(n, table, matrix, row + 1, left);
-  two = memoized_minimum_sum_descent(n, table, matrix, row + 1, right);
+  if (table[row][col] != -1)
+    return table[row][col];
 
-  if (one < two) {
-    minimum = one;
-  } else {
-    minimum = two;
-  }
+  a = memoized_minimum_sum_descent(n, table, matrix, row + 1, col);
+  b = memoized_minimum_sum_descent(n, table, matrix, row + 1, col + 1);
 
-  table[row][col] = matrix[row][col] + minimum;
+  table[row][col] = matrix[row][col] + (minimum(a, b));
 
   return table[row][col];
 }
 
+int tabulated_minimum_sum_descent(int n, int matrix[n][n], int row,
+                                  int column) {
+  int table[n + 1][n + 1];
+  int min = INT_MAX, a = 0, b = 0, minimum_sum_descent = INT_MAX;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (j == 0) {
+        if (i == 0) {
+          table[i][j] = matrix[i][j];
+        } else {
+          table[i][j] = matrix[i][j] + table[i - 1][0];
+        }
+        min = minimum(min, table[i][j]);
+        continue;
+      }
+      if (j == i) {
+        table[i][j] = matrix[i][j] + table[i - 1][j - 1];
+        min = minimum(min, table[i][j]);
+        break;
+      }
+      a = table[i - 1][j];
+      if (j == 0) {
+        min = minimum(a, min);
+      } else {
+        b = table[i - 1][j - 1];
+        min = minimum(a, b);
+      }
+      table[i][j] = matrix[i][j] + min;
+    }
+    // minimum_sum_descent = minimum(minimum_sum_descent, min);
+  }
+
+  minimum_sum_descent = 999999999;
+
+  for (int i = 0; i < n; i++) {
+    if (table[n - 1][i] < minimum_sum_descent)
+      minimum_sum_descent = table[n - 1][i];
+  }
+
+  return minimum_sum_descent;
+}
 //-------------------------------------------------------------------------
 //                          Minimum Sum Descent
 //-------------------------------------------------------------------------
@@ -171,7 +207,7 @@ int memoized_minimum_sum_descent(int n, int table[n][n], int matrix[n][n],
 //-------------------------------------------------------------------------
 //                          N choose K problem
 //-------------------------------------------------------------------------
-void nck_driver() {
+void choose_driver() {
   int n = 5;
   int k = 3;
   int table[n][k];
@@ -183,32 +219,41 @@ void nck_driver() {
     }
   }
 
-  for (int i = 0; i < n; i++) {
-    table[i][0] = 1;
-    table[i][1] = i;
-    // table[i][i] = 1;
-  }
-
-  printf("N choose k normal solution: %d\n\n", nck(n, k));
-  printf("N choose k memoized solution: %d\n\n", memoized_nck(n, k, table));
+  printf("N choose k normal solution: %d\n\n", choose(n, k));
+  printf("N choose k memoized solution: %d\n\n", memoized_choose(n, k, table));
+  printf("N choose k tabulated solution: %d\n\n", tabulated_choose(n, k));
 }
 
-int nck(int n, int k) {
-  if (k == 0)
-    return 1;
-  if (k == 1)
-    return n;
-  if (n == k)
+int choose(int n, int k) {
+  if (k == 0 || n == k)
     return 1;
 
-  return nck(n - 1, k - 1) + nck(n - 1, k);
+  return choose(n - 1, k - 1) + choose(n - 1, k);
 }
 
-int memoized_nck(int n, int k, int table[n][k]) {
+int memoized_choose(int n, int k, int table[n][k]) {
+  if (k == 0 || n == k)
+    return 1;
   if (table[n][k] != -1)
     return table[n][k];
 
-  table[n][k] = nck(n - 1, k - 1) + nck(n - 1, k);
+  table[n][k] = choose(n - 1, k - 1) + choose(n - 1, k);
+
+  return table[n][k];
+}
+
+int tabulated_choose(int n, int k) {
+  int table[n][k];
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < k; j++) {
+      if (k == 0 || k == n) {
+        table[n][k] = 1;
+        continue;
+      }
+      table[n][k] = table[n - 1][k - 1] + table[n - 1][k];
+    }
+  }
 
   return table[n][k];
 }
@@ -236,9 +281,6 @@ void rod_cutting_driver() {
   print_array(n, array);
   printf("Rod cutting driver solution: %d\n", rod_cutting(n, n - 1, array));
 }
-
-int count(int n, int value) { return n / value; }
-int max_rod_value(int n, int value) { return count(n, value) * value; }
 
 int rod_cutting(int n, int i, int rod_price[]) {
   int rod_cut = -1;
